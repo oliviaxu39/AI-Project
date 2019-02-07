b453576aaf48406b243bd04e35c123465b5b64fd
Margherita <mfirenze@parktudor.org>
2019-02-07 12:38:47 -0500

    update to make dumb TTTAI

--- src/Driver.java
+++ src/Driver.java
@@ -1,8 +1,10 @@
 public class Driver {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Bye");
-        System.out.println("test");
-        System.out.println("hola");
+        Player AIsimple = new TTTsimpleAI("X");
+        Board b1 = new TTTBoard();
+        System.out.println(b1.toString());
+
+
+
     }
 }
---
+++ src/TTTsimpleAI.java
@@ -0,0 +1,29 @@
+/**
+ * Created by margheritafirenze on 2/6/19.
+ */
+public class TTTsimpleAI extends Player {
+
+    /**
+     * Constucts a new HumanPlayer
+     * @param name
+     */
+    public TTTsimpleAI(String name) {
+        super(name);
+    }
+
+    /**
+     * getsMove
+     * @param board the board of the game
+     * @return location to move
+     */
+    public String getMove(Board board){
+        for(int n=0; n<2; n++){
+            if(board.streakInCol(n,"O",2)){
+                for(int x=0; x<2; x++){
+                    if(board.get(x, n).equals("X") || board.get(x, n).equals("O") );
+                }
+            }
+        }
+
+    }
+}
